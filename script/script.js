9b006a931f6c99631dab916931644c08311b85ab
Des7r0 <akamaru1337@gmail.com>
2020-11-22 03:54:12 +0300

    Урок номер 2

--- script/script.js
+++ script/script.js
@@ -1,9 +1,17 @@
-// let money = 
-// let income = 
-// let addExpenses = 
-// let deposit =
-// let mission =
-// let period = 
+  let money = 70000;
+  let income = 'фриланс';
+  let addExpenses = 'Интернет, Развлечения, Коммуналка'; 
+  let deposit = true;
+  let mission = 400000;
+  let period = 9;
 
-alert('Первый урок'); 
-console.log('Начало');
+console.log(typeof(money));
+console.log(typeof(income));
+console.log(typeof(deposit));
+console.log(addExpenses.length);
+console.log('Период равен ' + period + ' месяцев.' + ' Цель заработать '  + mission + ' рублей');
+console.log(addExpenses.toLowerCase());
+console.log(addExpenses.split(', '));
+
+let budgetDay = 70000 / 30 ;
+console.log(budgetDay);
