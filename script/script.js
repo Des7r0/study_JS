853f5d839eba1e35ef9717e73958af32df7b4e6f
Des7r0 <akamaru1337@gmail.com>
2020-11-25 21:54:25 +0300

    Исправил код

--- script/script.js
+++ script/script.js
@@ -1,10 +1,8 @@
   'use strict';
 
-let isNumber = function(n) {
+const isNumber = function(n) {
   return !isNaN(parseFloat(n)) && isFinite(n)
 };
-
-
 
   let money = 0;
   const income = 'фриланс';
@@ -13,23 +11,18 @@ let isNumber = function(n) {
   const mission = 400000;
   const period = 9;
 
-
-/*let start = function(){                    //функция проверит то, что ввел пользлватель
+const start = function(){                    //функция проверит то, что ввел пользлватель
   money = prompt('Ваш месячный доход?');
 
   while (!isNumber(money)) {   // если число NaN , если пустая строка , если ничего не ввели
   money = prompt('Ваш месячный доход?');
 }
 };  
-*/
 
 do {
 money = prompt('Ваш месячный доход?');
 }
 while (!isNumber(money));
-
-
-
 
 const showTypeOf = function(data) {
   console.log(typeof(data));
@@ -43,11 +36,9 @@ console.log('Возможные расходы: ' , addExpenses.split(', '));
 
 let expenses1,expenses2;
 
-let  getExpensesMonth = function () {
+const  getExpensesMonth = function () {
   let sum = 0;
-
-  for (let i = 0; i < 2; i++)    {               // prompt - наше условие 
-
+  for (let i = 0; i < 2; i++)  {               // prompt - наше условие 
       if (i === 0) {                                                   //когда i = 0 выполняется первая иттерация
         expenses1 = prompt('Введите обязательную статью расходов');
       } else if (i === 1) {                                             //когда i = 1 выполняется вторая иттерация
@@ -66,9 +57,7 @@ while (!isNumber(sum));
 
 
 const expensesAmount =  getExpensesMonth();
-
 console.log('Cумму всех обязательных расходов за месяц: ' , expensesAmount);
-
 
 const getAccumulatedMonth = function () {
   return money - expensesAmount;
@@ -77,7 +66,6 @@ let monthProfit =  getAccumulatedMonth();
 console.log('Накопления за месяц: ' , monthProfit);
 
 const accumulatedMonth = monthProfit ; 
-
 let budgetDay = accumulatedMonth / 30 ;
 console.log('Бюджет на день:', Math.floor(budgetDay));
 
@@ -97,7 +85,6 @@ console.log(getStatusIncome());
 const getTargetMonth = function () {
   return Math.ceil(mission/accumulatedMonth);
 }
-
 let target = getTargetMonth();
 
 if ( target > 0) {
