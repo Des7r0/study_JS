f4c806deb19f8d431cfbeb2f5438d0d11e76c81f
Des7r0 <akamaru1337@gmail.com>
2020-12-01 22:00:42 +0300

    Выполненное задание 9

--- script/script.js
+++ script/script.js
@@ -1,4 +1,26 @@
     'use strict';
+const buttonCalculate = document.getElementById('start'); 
+const buttonPlus1 = document.getElementsByTagName('button')[0];
+const buttonPlus2 = document.getElementsByTagName('button')[1];
+const checkBox = document.querySelector('#deposit-check');
+const additionalIncomeItem = document.querySelectorAll('.additional_income-item');
+const budgetDayValue = document.getElementByClassName('budget_day-value');
+const expensesMonthValue = document.getElementsByClassName('expenses_month-value');
+const additionalIncomeValue = document.getElementsByClassName('additional_income-value');
+const additionalExpensesValue = document.getElementsByClassName('additional_expenses-value');
+const incomePeriodValue = document.getElementsByClassName('income_period-value');
+const targetMonthValue = document.getElementsByClassName('target_month-value');
+const salaryAmount = document.querySelector('.salary-amount');
+const incomeTitle = document.querySelector('input.income-title');
+const incomeAmount = document.querySelector('.income-amount');
+const expensesTitle = document.querySelector('input.expenses-title');
+const expensesAmount = document.querySelector('.expenses-amount');
+const additionalExpensesItem = document.querySelector('.additional_expenses-item');
+const depositAmount = document.querySelector('.deposit-amount');
+const depositPercent = document.querySelector('.deposit-percent');
+const targetAmount = document.querySelector('.target-amount');
+const periodSelect = document.querySelector('.period-select');
+
 
 const isNumber = function(n) {
     return !isNaN(parseFloat(n)) && isFinite(n);
